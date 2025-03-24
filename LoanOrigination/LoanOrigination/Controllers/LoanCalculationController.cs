ea3baf6de84e8e7ccaec64b713178a7367712d4a
mohammed.labeeb@tavant.com <mohammed.labeeb@tavant.com>
2025-03-24 15:12:01 +0530

    Controllers

---
+++ LoanOrigination/LoanOrigination/Controllers/LoanCalculationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanOrigination.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoanCalculationController : ControllerBase
+    {
+    }
+}
