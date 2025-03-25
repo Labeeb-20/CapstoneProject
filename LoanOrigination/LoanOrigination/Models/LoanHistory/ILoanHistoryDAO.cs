6ae18d06ef77f58ce59d4069ef83aab7567285c5
Satyanarayana <satyanarayana.k@tavant.com>
2025-03-25 18:12:30 +0530

    c1

--- LoanOrigination/LoanOrigination/Models/LoanHistory/ILoanHistoryDAO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LoanOrigination.Models.LoanHistory
-{
-    public interface ILoanHistoryDAO
-    {
-        public List<LoanHistoryModel> GetLoanHistoryByCustomerId(int customerId);
-
-        public List<TransactionsModel> GetTransactionsByCustomerId(int customerId);
-    }
-}
