cf0bd4ccb51e6c18af834083da272748c6e809ff
mohammed.labeeb@tavant.com <mohammed.labeeb@tavant.com>
2025-03-26 10:03:15 +0530

    m2

--- LoanOrigination/LoanAppExceptionLib/CustomerNotFoundException.cs
+++ LoanOrigination/LoanAppExceptionLib/CustomerNotFoundException.cs
@@ -2,14 +2,6 @@
 {
     public class CustomerNotFoundException : Exception
     {
-<<<<<<< HEAD
         public CustomerNotFoundException(string message) : base(message) { }
-=======
-        public CustomerNotFoundException(string msg) : base(msg)
-        {
-            
-        }
-
->>>>>>> 49df8118a1ef4c20fa0747d90c882d3b12ca5f1d
     }
 }
--- LoanOriginationAngular/loan-origination-app/src/app/services/login/login.service.ts
+++ LoanOriginationAngular/loan-origination-app/src/app/services/login/login.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private apiUrl = 'http:/api/Account';
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, pin: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/Login/${username}/${pin}`, {});
+  }
 }
