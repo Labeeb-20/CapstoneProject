568df792aefe6920e9946678013e8dad491e733b
mohammed.labeeb@tavant.com <mohammed.labeeb@tavant.com>
2025-03-26 10:34:55 +0530

    m4

--- LoanOrigination/LoanOrigination/Exceptions/CustomerNotFoundException.cs
+++ LoanOrigination/LoanOrigination/Exceptions/CustomerNotFoundException.cs
@@ -3,9 +3,5 @@
     public class CustomerNotFoundException : Exception
     {
         public CustomerNotFoundException(string message) : base(message) { }
-<<<<<<< HEAD:LoanOrigination/LoanAppExceptionLib/CustomerNotFoundException.cs
-=======
-
->>>>>>> 94684ba1f285911f74026b2ddb452f7ee27aa7f8:LoanOrigination/LoanOrigination/Exceptions/CustomerNotFoundException.cs
     }
 }
