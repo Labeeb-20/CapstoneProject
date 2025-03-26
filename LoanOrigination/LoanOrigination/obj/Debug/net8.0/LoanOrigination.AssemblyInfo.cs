6502ea14b91a4edd3f43bc450f6e4d5991c8e36e
mohammed.labeeb@tavant.com <mohammed.labeeb@tavant.com>
2025-03-26 10:07:49 +0530

    m3

--- LoanOrigination/LoanOrigination/obj/Debug/net8.0/LoanOrigination.AssemblyInfo.cs
+++ LoanOrigination/LoanOrigination/obj/Debug/net8.0/LoanOrigination.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanOrigination")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1611bec679a2759bf1d708d35909612f7388a83f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49df8118a1ef4c20fa0747d90c882d3b12ca5f1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanOrigination")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanOrigination")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
