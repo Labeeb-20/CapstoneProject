d1b915b241ce3363ce5c1ca6c2d447396db17504
nandan.ds@tavant.com <nandan.ds@tavant.com>
2025-03-27 15:24:55 +0530

    c3

--- LoanOrigination/LoanTestPrj/FindCustomerTest.cs
+++ LoanOrigination/LoanTestPrj/FindCustomerTest.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LoanOrigination.Controllers;
+using LoanOrigination.Models.CustomerSearch;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
 
 namespace LoanTestPrj
 {
     public class FindCustomerTest
     {
+        [Fact]
+        public void GetCustomer_ReturnsOkResult_WhenCustomersExist()
+        {
+            // Arrange
+            var mockDal = new Mock<ICustomerDataAccess>();
+            var customers = new List<Customer>
+        {
+            new Customer {
+                Id = 1,
+                FirstName = "ac",
+                LastName = "abc",
+                Date_of_Birth = new DateOnly(2002, 10, 09),
+                Phone = "1234567890",
+                Email = "abc@123",
+                Address = "abc",
+                Company_Name = "abc",
+                Salary = 123,
+                Net_Income = 123,
+                Last_salary_date = new DateOnly(1990, 01, 02)
+            },
+            new Customer {
+                Id = 3,
+                FirstName = "abc",
+                LastName = "abc",
+                Date_of_Birth = new DateOnly(2002, 10, 09),
+                Phone = "string",
+                Email = "string",
+                Address = "string",
+                Company_Name = "string",
+                Salary = 0,
+                Net_Income = 0,
+                Last_salary_date = new DateOnly(2025, 03, 26)
+            }
+        };
+
+            mockDal.Setup(d => d.GetCustomer("abc", "abc", new DateOnly(2002, 10, 09))).Returns(customers);
+            var controller = new FindCustomerController(mockDal.Object);
+
+            // Act
+            var result = controller.GetCustomer("abc", "abc", new DateOnly(2002, 10, 09));
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedCustomers = Assert.IsType<List<Customer>>(okResult.Value);
+            Assert.Equal(2, returnedCustomers.Count);
+            Assert.Equal("ac", returnedCustomers[0].FirstName);
+        }
     }
+    
 }
--- LoanOrigination/LoanTestPrj/obj/Debug/net8.0/LoanTestPrj.AssemblyInfo.cs
+++ LoanOrigination/LoanTestPrj/obj/Debug/net8.0/LoanTestPrj.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanTestPrj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a41274645b1daf61cf87b47dba45bfdbe53d0e")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6e905b4c065876a46ba29bc9ead2c83c60c179")]
+>>>>>>> 0e55802 (c3)
 [assembly: System.Reflection.AssemblyProductAttribute("LoanTestPrj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanTestPrj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
