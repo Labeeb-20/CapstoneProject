70218ba55e8d77ce2fcd6773766a867244592e6a
Navanishwara <malave.navanishwara@tavant.com>
2025-03-27 10:47:20 +0530

    m3

--- LoanOriginationAngular/loan-origination-app/src/app/app.module.ts
+++ LoanOriginationAngular/loan-origination-app/src/app/app.module.ts
@@ -8,7 +8,16 @@ import { CustomersearchComponent } from './components/customersearch/customersea
 import { LoanhistoryComponent } from './components/loanhistory/loanhistory.component';
 import { ApplyloanComponent } from './components/applyloan/applyloan.component';
 import { DashboadComponent } from './components/dashboad/dashboad.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes : Routes=[
+  {
+    path : "",
+    component : CustomerdetailsComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +29,11 @@ import { DashboadComponent } from './components/dashboad/dashboad.component';
     DashboadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
